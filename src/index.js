72eac8a48045a0734b4079bd1a730d6bb4fa6f9e
JonECope <joecope63@gmail.com>
2019-09-24 23:19:26 -0500

    Added Game Over Screen

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@
 import BootScene from './scenes/BootScene.js';
 import Level1 from './scenes/Level1.js';
 import Config from './config/config.js';
+import GameOver from './scenes/GameOverScene.js'
 
 class Game extends Phaser.Game {
   constructor () {
     super(Config);
     this.scene.add('Boot', BootScene);
     this.scene.add('Level1', Level1);
-    this.scene.start('Boot');
+    this.scene.add('GameOver', GameOver);
+    this.scene.start('GameOver');
   }
 }
 

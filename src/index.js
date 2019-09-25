174b8fc3fcdf21952c067c39744a13f0ae1fdf64
Angelo Gomez <angelodgomez@gmail.com>
2019-09-24 22:56:59 -0500

    added successScene

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 /*global Phaser, window*/
 import BootScene from './scenes/BootScene.js';
 import Level1 from './scenes/Level1.js';
+import successScene from './scenes/successScene.js';
 import Config from './config/config.js';
+
 
 class Game extends Phaser.Game {
   constructor () {
     super(Config);
     this.scene.add('Boot', BootScene);
     this.scene.add('Level1', Level1);
+    this.scene.add('successScene', successScene);
     this.scene.start('Level1');
   }
 }

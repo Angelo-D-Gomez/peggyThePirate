82b7f1f7aec14177f215a4a595882fb9f2e2827f
Angelo Gomez <angelodgomez@gmail.com>
2019-09-29 13:05:54 -0500

    Added Boss Scene

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /*global Phaser, window*/
 import BootScene from './scenes/BootScene.js';
 import Level1 from './scenes/Level1.js';
+import Boss1 from './scenes/Boss1.js'
 import successScene from './scenes/successScene.js';
 import GameOver from './scenes/GameOverScene.js'
 import Config from './config/config.js';
@@ -14,6 +15,7 @@ class Game extends Phaser.Game {
     this.scene.add('Level1', Level1);
     this.scene.add('GameOver', GameOver);
     this.scene.add('successScene', successScene);
+    this.scene.add('Boss1', Boss1);
     this.scene.start('Level1');
   }
   }

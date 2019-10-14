d04ec24823efbc8405902ecbaf4e98996baf8e0c
Angelo Gomez <angelodgomez@gmail.com>
2019-10-13 21:48:51 -0500

    continued working on Lvl 1v2. Have started populating first section with enemies

--- src/scenes/Level1.1.js
+++ src/scenes/Level1.1.js
@@ -16,16 +16,24 @@ export default class Level1v2 extends Phaser.Scene {
       frameWidth: 32
     });
 
+    //projectiles
     this.load.image('bullet', './assets/sprites/bulletSmall.png');
+    this.load.image('coconut', './assets/sprites/coconut_small.png');
+
 
     // load background
-    this.load.image('beachBackground', './assets/Level1.1/beachArtwork.png')
+    this.load.image('beachBackground', './assets/Level1.1/beachArtwork.png');
+
+    //load enemies
+    this.load.image('monkey', './assets/Level1.1/monkey.png');
+    this.load.image('crab', './assets/Level1.1/crab_small.png');
+
 
 
     //load tile map
-    this.load.image('jungleTiles', './assets/Level1.1/placeholderTiles.png');
+    this.load.image('jungleTiles', './assets/Level1.1/jungleTileSheet.png');
     this.load.image('beachTiles', './assets/Level1.1/shipAndBeachTiles.png');
-    this.load.tilemapTiledJSON('map', './assets/Level1.1/Level1.json');
+    this.load.tilemapTiledJSON('map', './assets/Level1.1/Level 1 V2.json');
 
     // Load the gun/jump sound effect
     this.load.audio('gunAudio', './assets/audio/477346__mattiagiovanetti__some-laser-gun-shots-iii.mp3');
@@ -59,7 +67,7 @@ export default class Level1v2 extends Phaser.Scene {
 
     // tile map
     const map = this.make.tilemap({ key: 'map' });
-    var tileset1 = map.addTilesetImage('placeholderTiles', 'jungleTiles');
+    var tileset1 = map.addTilesetImage('jungleTileSheet', 'jungleTiles');
     var tileset2 = map.addTilesetImage('shipAndBeachTiles', 'beachTiles');
     const platforms = map.createStaticLayer('beach', tileset2, 0, 0);
     const platforms2 = map.createStaticLayer('jungle', tileset1, 0, 0);
@@ -78,9 +86,125 @@ export default class Level1v2 extends Phaser.Scene {
     }
   );
 
+  //add enemy's bullet group
+  this.enemyBullets = this.physics.add.group({
+    defaultKey: "bullet",
+    maxSize: 100
+});
+  //how to get gravity of bullets to be zero??
+  this.enemyBullets.children.iterate(function(child){
+});
+  //add enemy's coconut enemyGroup    //add enemy's bullet group
+      this.enemyCoconuts = this.physics.add.group({
+        defaultKey: "coconut",
+        maxSize: 100
+  });
+      //how to get gravity of bullets to be zero??
+      this.enemyCoconuts.children.iterate(function(child){
+  });
+
+    //create enemy group
+    this.enemyGroup = this.physics.add.group({});
+
+    //adding crab enemies
+    this.crab1 = this.physics.add.sprite(736, 544, 'crab');
+    this.enemyGroup.add(this.crab1);
+
+    //adding monkey enemies
+    this.monkey1 = this.physics.add.sprite(2288, 448, 'monkey');
+    this.enemyGroup.add(this.monkey1);
+    this.monkey2 = this.physics.add.sprite(2176, 288, 'monkey');
+    this.enemyGroup.add(this.monkey2);
+    this.monkey3 = this.physics.add.sprite(2340, 160, 'monkey');
+    this.enemyGroup.add(this.monkey3);
+
+
+    this.physics.add.collider(this.enemyGroup, platforms);
+    this.physics.add.collider(this.enemyGroup, platforms2);
+
+
+
+    //if player touches enemy
+    this.enemyGroup.children.each(
+          function (b) {
+            if (b.active) {
+              this.physics.add.overlap( //if enemyGroup touches player, calls function
+                b,
+                this.player,
+                this.gameOver,
+                null,
+                this
+              );
+            }
+          }.bind(this) //binds to each children
+        );
+
+
+
 
 
     // animations
+
+    //tweens
+
+    //crab walking movement
+    this.tweens.add({
+      targets: this.crab1,
+      x: '-=600',
+      ease: "Linear",
+      delay: 0,
+      duration: 5000,
+      yoyo: true,
+      repeat: -1,
+      flipX: true
+    });
+    //monkeys throw coconuts
+     this.tweens.add({
+      targets: this.monkey1,
+      ease: "Linear",
+      x: '-=0',
+      delay: 1000,
+      duration: 3000,
+      yoyo: true,
+      repeat: -1,
+      flipX: true,
+      onRepeat: function(){this.enemyShootTargeted(this.monkey1, this.enemyCoconuts)},
+     onRepeatScope: this
+    });
+    this.tweens.add({
+      targets: this.monkey2,
+      ease: "Linear",
+      x: '-=0',
+      delay: 1500,
+      duration: 3000,
+      yoyo: true,
+      repeat: -1,
+      flipX: true,
+      onRepeat: function(){this.enemyShootTargeted(this.monkey2, this.enemyCoconuts)},
+     onRepeatScope: this
+    });
+    this.tweens.add({
+      targets: this.monkey3,
+      ease: "Linear",
+      x: '-=0',
+      delay: 1250,
+      duration: 3000,
+      yoyo: true,
+      repeat: -1,
+      flipX: true,
+      onRepeat: function(){this.enemyShootTargeted(this.monkey3, this.enemyCoconuts)},
+     onRepeatScope: this
+    });
+
+
+
+
+
+
+
+
+
+
     // Peggy animations
     //create animation from spritesheet
   this.anims.create({
@@ -147,9 +271,9 @@ export default class Level1v2 extends Phaser.Scene {
       this.player.setVelocityY(300);
     }
 
-        var bang = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.O);
+    var bang = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.O);
 
-        if (Phaser.Input.Keyboard.JustDown(bang)){
+    if (Phaser.Input.Keyboard.JustDown(bang)){
           if(this.player.flipX == false){
             var velocity = {x: 1000, y: 0};
           }
@@ -165,7 +289,7 @@ export default class Level1v2 extends Phaser.Scene {
         }
 
         //player's bullet kills enemies
-            this.bullets.children.each(
+        this.bullets.children.each(
                   function (b) {
                     if (b.active) {
                       this.physics.add.overlap( //if bullet touches enemyGroup, calls function
@@ -182,14 +306,142 @@ export default class Level1v2 extends Phaser.Scene {
                       else if (b.y > 600) { //if bullet off bottom of screen
                         b.setActive(false);
                       }
-                      else if (b.x < 0){
+                      else if (b.x < this.player.x - 400){
                         b.setActive(false);
                       }
-                      else if (b.x > 800){
+                      else if (b.x > this.player.x + 400){
                         b.setActive(false);
                       }
                     }
                   }.bind(this) //binds to each children
                 );
+
+        //enemys's bullet kills player
+        this.enemyBullets.children.each(
+                              function (b) {
+                                if (b.active) {
+                                  this.physics.add.overlap( //if bullet touches player, calls function
+                                    b,
+                                    this.player,
+                                    this.hitPlayer,
+                                    null,
+                                    this
+                                  );
+                                  //refresh bullet group
+                                  if (b.y < 0) { //if bullet off top of screen
+                                    b.setActive(false);
+                                  }
+                                  else if (b.y > 600) { //if bullet off bottom of screen
+                                    b.setActive(false);
+                                  }
+                                  else if (b.x < 0){
+                                    b.setActive(false);
+                                  }
+                                  else if (b.x > 800){
+                                    b.setActive(false);
+                                  }
+                                }
+                              }.bind(this) //binds to each children
+                            );
+
+        this.enemyCoconuts.children.each(
+          function (b) {
+            if (b.active) {
+              this.physics.add.overlap( //if bullet touches player, calls function
+                b,
+                this.player,
+                this.hitPlayer,
+                null,
+                this
+              );
+              //refresh bullet group
+              if (b.y < 0) { //if bullet off top of screen
+                b.setActive(false);
+              }
+              else if (b.y > 1920) { //if bullet off bottom of screen
+                b.setActive(false);
+              }
+              else if (b.x < 0){
+                b.setActive(false);
+              }
+              else if (b.x > 8000){
+                b.setActive(false);
+              }
+            }
+          }.bind(this) //binds to each children
+        );
+
+      }
+
+
+      //function for enemy to shoot in a straight line, no aim
+      enemyShoot (enemy, bullets) {
+        console.log('enemy shoots!');
+        if(enemy.active){
+        if(enemy.flipX == true){
+          var velocity = {x: 700, y: 0};
+        }
+        else{
+          var velocity = {x: -700, y: 0};
+        }
+        var bullet = bullets.get();
+        bullet.enableBody(true, enemy.x, enemy.y, true, true)
+        .setVelocity(velocity.x, velocity.y);
+        bullet.body.setAllowGravity(false);
+      }
+      }
+
+
+      //targeted version of above function
+      enemyShootTargeted (enemy, bullets) {
+        console.log('enemy shoots, targeted!');
+        if(enemy.active){
+          var betweenPoints = Phaser.Math.Angle.BetweenPoints;
+      var angle = betweenPoints(enemy, this.player);
+      var velocityFromRotation = this.physics.velocityFromRotation;
+      //create variable called velocity from a vector2
+      var velocity = new Phaser.Math.Vector2();
+      velocityFromRotation(angle, 500, velocity);
+      //get bullet group
+        var bullet = bullets.get();
+        bullet.setAngle(Phaser.Math.RAD_TO_DEG * angle);
+        bullet.enableBody(true, enemy.x, enemy.y, true, true)
+        .setVelocity(velocity.x, velocity.y);
+        bullet.body.setAllowGravity(false);
+      }
+      }
+
+
+//triggers when enemy is hit
+hitEnemy(bullet, enemy){
+    console.log('hit');
+    enemy.disableBody(true, true);
+    bullet.disableBody(true, true);
+    //play hurt sound
+    //this.screamSound.play();
+
   }
+//triggers when player is hit
+hitPlayer(bullet, player){
+        console.log('hit');
+        player.disableBody(true, true);
+        bullet.disableBody(true, true);
+        // Play hurt Sound
+        //this.screamSound.play();
+        this.gameOver();
+      }
+
+
+
+
+
+  //end game, goes to game over scene
+  gameOver(){
+    // Stop music if playing
+  //this.gameMusic.stop();
+  console.log('game over!');
+  this.scene.start('GameOver');
+  }
+
+
 }

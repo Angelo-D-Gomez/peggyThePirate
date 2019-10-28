2915e7d38fe6d0e3fbb0c4aa5f7e626330e87b56
JonECope <joecope63@gmail.com>
2019-10-27 20:37:28 -0500

    introduction file made, no code yet

---
+++ src/scenes/Introduction.js
@@ -0,0 +1,33 @@
+/*global Phaser*/
+import * as ChangeScene from'./Changescene.js';
+export default class IntroScene extends Phaser.Scene {
+  constructor () {
+    super('IntroScene');
+  }
+
+  init (data) {
+    // Initialization code goes here
+
+  }
+
+  preload () {
+    // Preload assets
+
+  }
+  create (data) {
+    // add event addSceneEventListeners
+    //this.cameras.main.setBackgroundColor('#150033');
+
+    //Create the scene
+
+
+  }
+
+  update (time, delta) {
+    // Update the scene
+
+
+
+
+  }
+}

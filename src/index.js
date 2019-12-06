8a6f8654ff8ddda2b00ef0649f0ad896648af98f
JonECope <joecope63@gmail.com>
2019-12-06 11:55:17 -0600

    Fixed missing semicolons and extra filename changes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 /*global Phaser, window*/
 import BootScene from './scenes/BootScene.js';
 import Level1 from './scenes/Level1.js';
-import Boss1 from './scenes/Boss1.js'
+import Boss1 from './scenes/Boss1.js';
 import SuccessScene from './scenes/SuccessScene.js';
-import GameOver from './scenes/GameOverScene.js'
+import GameOver from './scenes/GameOverScene.js';
 import Config from './config/config.js';
 import Level1v2 from './scenes/Level1.1.js';
 import IntroScene from './scenes/Introduction.js';
-import BossIntroScene from './scenes/BossIntroduction.js'
+import BossIntroScene from './scenes/BossIntroduction.js';
 //import test from './scenes/test.js';
-import CutScene from './scenes/Cutscene.js';
-import GameWorld from './scenes/GameWorld.js'
+import CutScene from './scenes/CutScene.js';
+import GameWorld from './scenes/GameWorld.js';
 
 
 
